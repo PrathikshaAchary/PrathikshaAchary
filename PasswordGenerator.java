882daa1d28aeeb05a24056fed278ea52ef541d52
PrathikshaAchary <prathiksha.23cs102@sode-edu.in>
2024-10-03 10:14:44 +0530

    Add files via upload

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -1,21 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package passwordgenerator;
 
-/**
- *
- * @author smv
- */
-public class PasswordGenerator {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import java.util.Random;
+public class PasswordGenerator{
+    public static void main(String[] args){
+        String upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lower="abcdefghijklmnopqrstuvwxyz";
+        String nums="0123456789";
+        String all=upper+lower+nums;
+        String s1="";
+        Random rand1=new Random();
+        int r1=rand1.nextInt(26);
+        s1=s1+upper.substring(r1,r1+1)+lower.substring(r1,r1+1)+nums.substring(1,5);
+        
+        System.out.println(s1);
     }
-    
 }
