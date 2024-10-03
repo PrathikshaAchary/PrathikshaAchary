e379e9ec02978d722965781b97b5b98749dc9b08
PrathikshaAchary <prathiksha.23cs102@sode-edu.in>
2024-10-03 09:53:20 +0530

    Add files via upload

---
+++ PasswordGenerator.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author smv
+ */
+public class PasswordGenerator {
+    public static void main(String[] args){
+        String s1="Ab2";
+        System.out.println("Password is: ");
+        System.out.println(s1);
+    }
+}
