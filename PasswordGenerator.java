43bfd6f57a3b4d99ce3f97f46a0e82dcd7f5d469
PrathikshaAchary <prathiksha.23cs102@sode-edu.in>
2024-10-03 10:13:41 +0530

    Add files via upload

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -3,15 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package passwordgenerator;
 
 /**
  *
  * @author smv
  */
 public class PasswordGenerator {
-    public static void main(String[] args){
-        String s1="Ab2";
-        System.out.println("Password is: ");
-        System.out.println(s1);
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
+    
 }
