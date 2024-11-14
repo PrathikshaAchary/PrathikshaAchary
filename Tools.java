3ae2bc51bddf08097bbb7aa9d13173be676b3bc8
PrathikshaAchary <prathiksha.23cs102@sode-edu.in>
2024-11-14 10:12:38 +0530

    Add files via upload

---
+++ Tools.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1;
+
+/**
+ *
+ * @author smv
+ */
+public class Tools {
+    public void numbers(int n1){
+        for(int i=0;i<n1;i++){
+        System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public void even(int n1){ 
+        
+        for(int i=0;i<=n1;i++){
+            System.out.print(i*2+"  ");
+        }
+         System.out.println();
+    }
+    public void odd(int n1){ 
+        
+        for(int i=0;i<=n1;i++){
+            System.out.print(i*2+1+" ");
+        }
+        System.out.println();
+    }
+       public void mul3(int n1){ 
+         
+        for(int i=0;i<=n1;i++){
+            System.out.print(3*i+" ");
+        }
+        System.out.println();
+       }
+          public void mul4(int n1){ 
+       
+        for(int i=0;i<=n1;i++){
+            System.out.print(4*i+" ");
+        }
+          System.out.println();
+          }
+             public void mul5(int n1){ 
+        
+        for(int i=0;i<=n1;i++){
+            System.out.print(5*i+" ");
+        }
+        System.out.println();
+             }
+       
+        }
+        
+    
